370a835f35a94f9ac9b4d2d71ea112e215693f84
Johnny Dong <johnny.dong@up.education>
2025-04-05 19:28:46 +1300

    Contains Duplicate alternative solution using HashMap

--- Easy/217. Contains Duplicate.js
+++ Easy/217. Contains Duplicate.js
@@ -28,3 +28,31 @@ class Solution {
 // Time Complexity: O(n)
 // - We iterate through the array once, where `n` is the length of the array. 
 
+
+// Alternative Solution using HashMap - O(n) time complexity
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let map = new Map();
+
+    for(let i = 0; i < nums.length; i++)
+    {
+        if(map.has(nums[i]))
+        {
+            map.set(nums[i], map.get(nums[i]) + 1);
+        } else {
+            map.set(nums[i], 1);
+        }
+    }
+
+    for (let [key, value] of map)
+    {
+        if (value > 1)
+        {
+            return true;
+        }
+    }
+    return false;
+};
