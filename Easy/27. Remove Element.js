4d78c1333f48494be28d032b2719eeaabd4016be
Johnny Dong <johnny.dong@up.education>
2025-04-07 09:41:41 +1200

    Update Remove Element

--- Easy/27. Remove Element.js
+++ Easy/27. Remove Element.js
@@ -4,20 +4,14 @@
  * @return {number}
  */
 
-var removeElement = function(nums, val) { 
-    let index = 0; // Pointer to track where the next non-'val' element should be placed.
-
-    // Loop through the entire array
+var removeElement = function(nums, val) {
     for (let i = 0; i < nums.length; i++) {
-        // If the current element is NOT equal to 'val', we keep it
-        if (nums[i] !== val) {
-            nums[index] = nums[i]; // Move the non-'val' element to the 'index' position
-            index++; // Increment 'index' to point to the next insertion position
+        if (nums[i] === val) {
+            nums.splice(i, 1);
+            i--; // Decrement is necessary because after splice, the array shifts and we need to re-check the current index
         }
     }
-    
-    // 'index' now represents the new length of the modified array (excluding 'val' elements)
-    return index;
+    return nums.length;
 };
 
 // Time complexity is O(n) - where n is the size of the input array
