f931c7191dcffe3c7a9ec87b12be7bb1926f21cb
Johnny Dong <johnny.dong@up.education>
2025-04-07 09:52:32 +1200

    Another solution to Intersection of Two Arrays

--- Easy/349. Intersection of Two Arrays.js
+++ Easy/349. Intersection of Two Arrays.js
@@ -3,6 +3,31 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
+
+
+// Example 1:
+
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2]
+
+
+var intersection = function(nums1, nums2) {
+    let intersection = [];
+
+   let set1 = new Set(nums1); // [1,2]
+   let set2 = new Set(nums2); // [2]
+
+   for(let element of set1)
+   {
+       if(set2.has(element))
+       {
+           intersection.push(element);
+       }
+   }
+   return intersection;
+};
+
+
 var intersection = function(nums1, nums2) {
     let set1 = new Set(nums1); // set1 = [1,2]
     let unique = new Set(); // unique = [2]
